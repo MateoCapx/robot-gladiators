0117bb5fb32ea9c30390d2bb5f2136129310dcbe
Mateo <mateom.realtor@gmail.com>
2022-04-25 15:58:29 -0400

    Create Function

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
-let x = 12;
 
-if (x =12 ){
-    console.log("WORKS")
+window.prompt("What is your robot's name? ");
+
+let playerName= window.prompt("What is your robot's name? ")  // This is how we store user input for the program
+
+window.alert(playerName);  // Added an alert that will contain the robot's name after it's entered.
+
+// this creates a function named "fight"
+
+function fight(){
+window.alert(" The Fight has begun!");
 }
+fight();
+ 
+console.log(playerName);
+
+// // "Tony the Robot is ready for battle!"
+// console.log(playerName + " is ready for battle!");   // Example of string concatenation 
+
+// // "Your robot, Tony the Robot, has won!
+// console.log("Your robot, " + playerName + ", has won!");      // Example of string concatenation 
