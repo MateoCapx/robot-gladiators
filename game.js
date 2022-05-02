3f5218c3db4d67683a2d819549d4c9ee82e8c515
Mateo <mateom.realtor@gmail.com>
2022-05-01 20:36:08 -0400

    shop function

--- game.js
+++ game.js
@@ -75,6 +75,81 @@ let fight = function(enemyName) {
   }; // end of fight function
 
 
+// Shop feature Function
+
+let shop = function() {
+    // ask player what they'd like to do
+    let shopOptionPrompt = window.prompt(
+      "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+  
+
+
+// use switch to carry out action
+switch (shopOptionPrompt) {
+
+
+    case "REFILL": // new case
+    case "refill":
+
+        if (playerMoney >= 7) {
+            window.alert("Refilling player's health by 20 for 7 dollars."); 
+        
+      // increase health and decrease money
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+      break;
+}
+
+else {
+    window.alert ("You dont have enough money!")
+}
+
+    case "UPGRADE": // new case
+    case "upgrade":
+
+        //check to make sure player have enough money to shop !
+
+        if (playerMoney >= 7) {
+            window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+     
+  
+      // increase attack and decrease money
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+
+          }  
+
+          else {
+            window.alert("You don't have enough money!");
+          }
+
+      break;
+
+        
+
+
+    case "LEAVE": // new case
+    case "leave":
+      window.alert("Leaving the store.");
+  
+      // do nothing, so function will end
+      break;
+
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+  
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+};
+
+
+
+
+
 
   let startGame =function() {
 
@@ -101,6 +176,24 @@ for(let i=0; i<enemyNames.length; i++ ){
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
   }
+// if player is still alive and we're not at the last enemy in the array
+if (playerHealth > 0 && i < enemyNames.length - 1) {
+      // ask if player wants to use the store before next round
+  let storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+  // if yes, take them to the store() function
+  if (storeConfirm) {
+    shop();
+    }
+  }
+
+// if we're not at the last enemy in the array  <------> (-1 will always give us the last index)
+  if ( i< enemyNames.length -1){
+  
+  shop();
+
+}
+
   // if player isn't alive, stop the game
   else {
     window.alert('You have lost your robot in battle! Game Over!');
@@ -110,6 +203,7 @@ for(let i=0; i<enemyNames.length; i++ ){
 } // End startGame Funtion 
 
 startGame();
+
 
 
 let endGame = function() {
